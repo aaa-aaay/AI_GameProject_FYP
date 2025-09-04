250ea6fcdcf9cdabd80f661f62f6db12f91610ca
Freddy6245 <kaylentan123@gmail.com>
2025-09-04 17:50:03 +0800

    work on wall detection tmr

--- Assets/Code/Kaylen/PlayerController.cs
+++ Assets/Code/Kaylen/PlayerController.cs
@@ -1,16 +1,50 @@
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Movement Settings")]
+    public float moveSpeed = 5f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 1.5f;
+
+
+    private CharacterController controller;
+    private Vector3 velocity;
+    private float xRotation = 0f;
+
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
+
+        // Lock the cursor
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Move();
     }
+
+    void Move()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * moveSpeed * Time.deltaTime);
+
+        // Gravity
+        if (controller.isGrounded && velocity.y < 0)
+            velocity.y = -2f;
+
+        if (Input.GetButtonDown("Jump") && controller.isGrounded)
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+   
 }
