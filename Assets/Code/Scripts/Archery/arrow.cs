6925687e2dc5c927c1722ff980e09e0fa3d51e3a
radiumerst <foongwenwei@gmail.com>
2025-10-03 09:36:47 +0800

    Fix arrow hit behaviour

--- Assets/Code/Scripts/Archery/arrow.cs
+++ Assets/Code/Scripts/Archery/arrow.cs
@@ -64,7 +64,10 @@ public class arrow : MonoBehaviour
         Vector3 delta = desiredTipPos - tip.position;
         transform.position += delta;
 
-        transform.parent = collision.gameObject.transform;
+        if (target)
+            transform.parent = collision.transform.parent.transform;
+        else
+            transform.parent = collision.gameObject.transform;
 
         int point;
         if (target) point = target.OnHit(contact); else point = 0;
