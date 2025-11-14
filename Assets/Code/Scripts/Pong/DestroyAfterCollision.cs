c8241e2171951a32ad751d1e94f08b1160a9d146
aaa-aaay <118505992+aaa-aaay@users.noreply.github.com>
2025-11-14 12:51:59 +0800

    fixed collision issue

--- Assets/Code/Scripts/Pong/DestroyAfterCollision.cs
+++ Assets/Code/Scripts/Pong/DestroyAfterCollision.cs
@@ -1,9 +1,16 @@
+using System.Collections;
 using UnityEngine;
 
 public class DestroyAfterCollision : MonoBehaviour
 {
     private void OnCollisionExit(Collision collision)
     {
+        StartCoroutine(DelayDestroy());
+    }
+
+    IEnumerator DelayDestroy()
+    {
+        yield return null;
         Destroy(gameObject);
     }
 }
